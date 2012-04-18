847ac2dc37ea7f9edfb4ae7ffe3feaa7189d6814
Kostis Karantias <karantiaskostis@gmail.com>
2012-04-18 12:14:25 +0000

    Moar optimized 42

--- pe42.py
+++ pe42.py
@@ -1,15 +1,14 @@
 from fileinput import input
+from math import sqrt
 
-words = input( 'input/pe42.txt' )[ 0 ].split( ',' )
-words = [ word[ 1:-1 ] for word in words ] #remove quotes
+#Remove quotes from original input
+words = [ word[ 1:-1 ] for word in input( 'input/pe42.txt' )[ 0 ].split( ',' ) ]
 
-#Upper bound for triangle numbers is 26 times the maximum length of any word
-upper = max( [ len( word ) for word in words ] ) * 26 + 1
-
-#Triangles will contain all the triangle numbers from first to upper-th
-triangles = set( [ x * ( x + 1 ) / 2 for x in xrange( 1, upper ) ] )
-
-#String ord (sum of all character's ords)
+#String ord (sum of all characters' ords)
 sord = lambda s: sum( [ ord( ch ) - 64 for ch in s ] )
 
-print sum( [ sord( word ) in triangles for word in words ] )
+#Triangle is any number that evaluates ( sqrt( 1 + 8 * n ) + 1 ) / 2
+isint = lambda n: int( n ) == n
+triangle = lambda n: isint( ( sqrt( 1 + 8 * n ) + 1 ) / 2 )
+
+print sum( [ triangle( sord( word ) ) for word in words ] )
