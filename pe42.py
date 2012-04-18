b3fdb9dce89b37eb56832129c5aa874f905ce3f4
Kostis Karantias <karantiaskostis@gmail.com>
2012-04-18 12:21:11 +0000

    REMOVE ALL THE COMMENTS

--- pe42.py
+++ pe42.py
@@ -1,13 +1,7 @@
 from fileinput import input
 from math import sqrt
 
-#Remove quotes from original input
 words = [ word[ 1:-1 ] for word in input( 'input/pe42.txt' )[ 0 ].split( ',' ) ]
-
-#String ord (sum of all characters' ords)
 sord = lambda s: sum( [ ord( ch ) - 64 for ch in s ] )
-
-#Triangle is any number that evaluates ( sqrt( 1 + 8 * n ) + 1 ) / 2
 triangle = lambda n: ( ( sqrt( 1 + 8 * n ) + 1 ) / 2 ).is_integer()
-
 print sum( [ triangle( sord( word ) ) for word in words ] )
