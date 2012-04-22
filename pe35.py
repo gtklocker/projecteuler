58e0f306b5d148355ad1124446ccfefc65623064
Kostis Karantias <karantiaskostis@gmail.com>
2012-04-22 16:57:15 +0000

    35: Clarification on coding style, 79 chars per line

--- pe35.py
+++ pe35.py
@@ -9,10 +9,16 @@ def rotate(n):
     return ret
 
 def main():
-    "Solution for Problem 35"
+    """Solution for Problem 35:
+    This code is different from anything else on this repo as far as coding
+    style goes and that's because I tried following the Google Python Coding
+    Style: http://google-styleguide.googlecode.com/svn/trunk/pyguide.html
+
+    """
     upper = 1000000
 
-    #When generating primes, we need to know the upper bound which is defined by our maximum rotation
+    #When generating primes, we need to know the upper bound which is defined by
+    #our maximum rotation
     max_perm = max(max(rotate(upper)), max(rotate(upper - 1)))
 
     #Array that will tell whether a number is prime or not
@@ -26,14 +32,14 @@ def main():
                 break
             prime[i * j] = False
 
-    found = set() #Set of all the circular primes
+    found = [] #List of all the circular primes
     for i in range(2, upper):
         if prime[i]:
             try:
                 for r in rotate(i):
                     if not prime[r]:
                         raise Exception
-                found.add(i)
+                found.append(i)
             except:
                 continue
     print len(found)
