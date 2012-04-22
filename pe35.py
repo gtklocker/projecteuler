fab29fa4090b78e71fdb8bdf3f080513f7e38d12
Kostis Karantias <karantiaskostis@gmail.com>
2012-04-22 00:22:00 +0000

    35

---
+++ pe35.py
@@ -0,0 +1,42 @@
+from math import ceil, sqrt
+
+def rotate(n):
+    "Rotate n according to the problem statement"
+    n = str(n)
+    ret = []
+    for i in range(len(n)):
+        ret.append(int(n[i] + n[i + 1:] + n[:i]))
+    return ret
+
+def main():
+    "Solution for Problem 35"
+    upper = 1000000
+
+    #When generating primes, we need to know the upper bound which is defined by our maximum rotation
+    max_perm = max(max(rotate(upper)), max(rotate(upper - 1)))
+
+    #Array that will tell whether a number is prime or not
+    prime = [True] * (max_perm + 1)
+    prime[0] = False
+    prime[1] = False
+
+    for i in range(2, int(ceil(sqrt(max_perm + 1))) + 1):
+        for j in range(2, max_perm + 1):
+            if i * j >= max_perm:
+                break
+            prime[i * j] = False
+
+    found = set() #Set of all the circular primes
+    for i in range(2, upper):
+        if prime[i]:
+            try:
+                for r in rotate(i):
+                    if not prime[r]:
+                        raise Exception
+                found.add(i)
+            except:
+                continue
+    print len(found)
+
+if __name__ == '__main__':
+    main()
