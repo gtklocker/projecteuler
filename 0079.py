8f4ada5160b88b7094086a95c6dfae3306997ffa
Kostis Karantias <karantiaskostis@gmail.com>
2012-05-20 14:23:44 -0400

    Improved 79.

--- 0079.py
+++ 0079.py
@@ -1,30 +1,32 @@
-key = []
-
-def findpos(n):
+def findpos(key, n):
     try:
         return key.index(n)
     except:
         if n == 0:
             return -1
-        return findpos(n - 1)
+        return findpos(key, n - 1)
 
-with open('keylog.txt') as fp:
-    attempts = fp.read().split('\r\n')[:-1]
+def main():
+    key = []
+    with open('keylog.txt') as fp:
+        attempts = fp.read().split('\r\n')[:-1]
 
-for attempt in attempts:
-    for ns in attempt:
-        ns = int(ns)
-        if not ns in key:
-            key.append(ns)
+    for attempt in attempts:
+        pos = []
+        for ns in attempt:
+            ns = int(ns)
+            pos.append(findpos(key, ns))
+            if not ns in key:
+                key.append(ns)
 
-    pos_a = findpos(int(attempt[0]))
-    pos_b = findpos(int(attempt[1]))
-    pos_c = findpos(int(attempt[2]))
-    if pos_a > pos_b:
-        key[pos_a], key[pos_b] = key[pos_b], key[pos_a]
-    if pos_b > pos_c:
-        key[pos_b], key[pos_c] = key[pos_c], key[pos_b]
-    if pos_a > pos_c:
-        key[pos_a], key[pos_c] = key[pos_c], key[pos_a]
+        if pos[0] > pos[1]:
+            key[pos[0]], key[pos[1]] = key[pos[1]], key[pos[0]]
+        if pos[1] > pos[2]:
+            key[pos[1]], key[pos[2]] = key[pos[2]], key[pos[1]]
+        if pos[0] > pos[2]:
+            key[pos[0]], key[pos[2]] = key[pos[2]], key[pos[0]]
 
-print ''.join([str(c) for c in key])
+    print ''.join([str(c) for c in key])
+
+if __name__ == '__main__':
+    main()
