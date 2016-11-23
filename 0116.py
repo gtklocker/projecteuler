d94ebc0ded21a61f8727c8e1b34ad1cf98c71ee4
Kostis Karantias <karantiaskostis@gmail.com>
2016-11-23 20:42:14 +0200

    Improved solution for 116

--- 0116.py
+++ 0116.py
@@ -1,11 +1,14 @@
 def solve(n, l):
-    dp = [0] * n
-    for i in range(n - 1, -1, -1):
-        if i + l <= n:
-            dp[i] += 1
-            for j in range(i + l, n):
-                dp[i] += dp[j]
-    return sum(dp)
+    dp = [[0] * (n + 1) for i in range(2)]
+    dp[0][0] = 1
+
+    for i in range(n + 1):
+        if i - l >= 0:
+            dp[1][i] = dp[1][i - l] + dp[0][i - l]
+        if i - 1 >= 0:
+            dp[0][i] = dp[1][i - 1] + dp[0][i - 1]
+
+    return dp[0][n] + dp[1][n] - 1
 
 n = 50
 sol = solve(n, 2) + solve(n, 3) + solve(n, 4)
